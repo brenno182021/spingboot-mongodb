b75acdbc070f70ae78cf9d3147cc7fcf89ee208e
Glayson.Santos <glaysonbrennod.santos@gmail.com>
2024-09-22 13:14:47 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/springboot/mongodb/services/UserService.java
+++ src/main/java/com/springboot/mongodb/services/UserService.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.mongodb.client.model.geojson.GeoJsonObjectType;
 import com.springboot.mongodb.domain.User;
 import com.springboot.mongodb.dto.UserDTO;
 import com.springboot.mongodb.repository.UserRepository;
@@ -29,6 +28,11 @@ public class UserService {
 	
 	public User insert(User obj) {
 		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
 	}
 	
 	public User fromDTO(UserDTO objDto) {
